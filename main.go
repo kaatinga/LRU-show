b5b9d3ec1b5f25e30a2ee8397635cb50d440fcc5
kaatinga <54396113+kaatinga@users.noreply.github.com>
2020-07-24 11:49:58 +0300

    the app is updated followwing the new model

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		err error
 	)
 
-	LRU.cache, err = NewCache(3, 3)
+	LRU.cache, err = NewCache(3)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -39,8 +39,7 @@ func main() {
 	}
 
 	LRU.cacheData = tview.NewTable()
-	LRU.queueData = tview.NewTable()
-	LRU.queueOrderData = tview.NewTable()
+	LRU.orderData = tview.NewTable()
 
 	messageLog := tview.NewTable()
 	inputField := tview.NewInputField().
@@ -51,20 +50,18 @@ func main() {
 	// Grid Layout
 	grid := tview.NewGrid().
 		SetRows(1, 0, 1).
-		SetColumns(35, 25, 25, 0).
+		SetColumns(25, 40, 0).
 		SetBorders(true)
 
 	// titles
-	grid.AddItem(title("=== Cache ==="), 0, 0, 1, 1, 0, 0, false).
-		AddItem(title("=== queue ==="), 0, 1, 1, 1, 0, 0, false).
-		AddItem(title("=== queue Order ==="), 0, 2, 1, 1, 0, 0, false).
-		AddItem(title("=== Message Log ==="), 0, 3, 1, 1, 0, 0, false)
+	grid.AddItem(title("== Cache (No order) =="), 0, 0, 1, 1, 0, 0, false).
+		AddItem(title("== Order (The oldest in the down) =="), 0, 1, 1, 1, 0, 0, false).
+		AddItem(title("== Message Log =="), 0, 2, 1, 1, 0, 0, false)
 
 	grid.AddItem(LRU.cacheData, 1, 0, 1, 1, 0, 0, false).
-		AddItem(LRU.queueData, 1, 1, 1, 1, 0, 0, false).
-		AddItem(LRU.queueOrderData, 1, 2, 1, 1, 0, 0, false).
-		AddItem(messageLog, 1, 3, 1, 1, 0, 0, false).
-		AddItem(inputField, 2, 0, 1, 4, 0, 0, true)
+		AddItem(LRU.orderData, 1, 1, 1, 1, 0, 0, false).
+		AddItem(messageLog, 1, 2, 1, 1, 0, 0, false).
+		AddItem(inputField, 2, 0, 1, 3, 0, 0, true)
 
 	// submitted is toggled each time Enter is pressed
 	// var submitted bool
@@ -88,53 +85,33 @@ func main() {
 			// AddToQueue a message to the log
 			messageRow = AddMessage(messageLog, strings.Join([]string{"The user entered expression", expression}, ": "), messageRow)
 
-			// Start to work with cache and queue
-
-			if !LRU.cache.HasResult(expression) {
+			// Start to work with cache
+			if !LRU.cache.Increment(expression) {
 
 				// Calculate the result
 				var result int64
 				result, err = calc.Calc(expression)
 				if err != nil {
-					messageRow = AddMessage(messageLog, err.Error(), messageRow)
+					messageRow = AddMessage(messageLog, "Error: "+err.Error(), messageRow)
 					break
 				}
 				messageRow = AddMessage(messageLog, strings.Join([]string{"The expression result was calculated", strconv.Itoa(int(result))}, ": "), messageRow)
 
-				if !LRU.cache.CheckSpaceAndAddToCache(expression, result, 1) {
-					messageRow = AddMessage(messageLog, "The cache has no space to add this expression", messageRow)
-
-					// Work with queue
-					count := LRU.cache.HasExpression(expression)
-					if count != 0 {
-						messageRow = AddMessage(messageLog, "The queue has this expression. The counter was incremented: "+strconv.Itoa(int(count)), messageRow)
-						if count > LRU.cache.GetMinCount() {
-
-							err = LRU.cache.Move(expression, result, count)
-							if err != nil {
-								messageRow = AddMessage(messageLog, err.Error(), messageRow)
-								break
-							}
-						} else {
-							messageRow = AddMessage(messageLog, "No need to move to cache", messageRow)
-						}
-					} else {
-						messageRow = AddMessage(messageLog, "The queue has not such an expression. The expression is new!", messageRow)
-						err = LRU.cache.AddToQueue(expression, &item{Count: 1})
-						if err != nil {
-							messageRow = AddMessage(messageLog, err.Error(), messageRow)
-							break
-						}
-					}
+				if !LRU.cache.Add(expression, result) {
+					messageRow = AddMessage(messageLog, "The cache had no free space", messageRow)
+					messageRow = AddMessage(messageLog, "The oldest item was substituted with the new one", messageRow)
 				} else {
-					messageRow = AddMessage(messageLog, "The expression was added to the cache. The cache is not full", messageRow)
+					messageRow = AddMessage(messageLog, "The expression was added to the cache. The cache was not full", messageRow)
 				}
 			} else {
 				messageRow = AddMessage(messageLog, strings.Join([]string{"The result was found in the cache", strconv.Itoa(int(LRU.cache.items[expression].result))}, ": "), messageRow)
 			}
 
 			// print the cache onscreen
-			LRU.PrintLRU()
+			printMessage := LRU.PrintLRU()
+			if printMessage != "" {
+				messageRow = AddMessage(messageLog, printMessage, messageRow)
+			}
 
 			//	// Create a modal dialog
 			//	m := tview.NewModal().
