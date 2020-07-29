951b20155da27313571ad488764b2d241737e42f
kaatinga <54396113+kaatinga@users.noreply.github.com>
2020-07-29 13:18:07 +0300

    the head and tail of the cache are now indicated in the Cache column

--- print.go
+++ print.go
@@ -23,14 +23,19 @@ func (display *LRUMonitor) PrintCache() (message string) {
 	display.cacheData.SetCellSimple(i, 2, display.cache.GetTheOldestIndex())
 	i++
 	display.cacheData.SetCellSimple(i, 1, "Count: ")
-	display.cacheData.SetCellSimple(i, 2, strconv.Itoa(int(display.cache.GetMinCount())))
+	display.cacheData.SetCellSimple(i, 2, strconv.Itoa(int(display.cache.GetTheOldestCount())))
 	i++
 	display.cacheData.SetCellSimple(i, 0, "Capacity: ")
 	display.cacheData.SetCellSimple(i, 1, strconv.Itoa(int(capacity)))
 	i++
 	display.cacheData.SetCellSimple(i, 0, "Size: ")
 	display.cacheData.SetCellSimple(i, 1, strconv.Itoa(int(display.cache.GetTheCacheSize())))
-
+	i++
+	display.cacheData.SetCellSimple(i, 0, "Head: ")
+	display.cacheData.SetCellSimple(i, 1, display.cache.GetTheHeadIndex())
+	i++
+	display.cacheData.SetCellSimple(i, 0, "Tail: ")
+	display.cacheData.SetCellSimple(i, 1, display.cache.GetTheOldestIndex())
 	// Reuse i
 	i = 0
 	display.orderData.Clear()
